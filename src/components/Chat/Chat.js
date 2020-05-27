512b9b08a47557f73192052d6ad21f4698dcc16c
aviharez <elzainsyifa@gmail.com>
2020-05-27 18:10:31 +0700

    change endpoint to heroku

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({ location }) => {
     const [room, setRoom] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://zeta-chat.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
